dd280deda68ef634302055543be3e5601066e226
Mikko Saari <mikko@mikkosaari.fi>
2020-09-08 10:36:12 +0300

    Renames medium.js to rssfeed.js

---
+++ site/_data/rssfeed.js
@@ -0,0 +1,27 @@
+var axios = require("axios")
+var toJSON = require("xml2js").parseString
+
+var url =
+	process.env.RSS_FEED ||
+	"https://feeds.soundcloud.com/users/soundcloud:users:201911010/sounds.rss"
+
+module.exports = () => {
+	return new Promise((resolve, reject) => {
+		axios
+			.get(url)
+			.then((response) => {
+				// turn the feed XML into JSON
+				toJSON(response.data, function (err, result) {
+					// create a path for each item based on Medium's guid URL
+					result.rss.channel[0].item.forEach((element) => {
+						var url = element.link[0].split("/")
+						element.path = url[url.length - 1].split("?")[0]
+					})
+					resolve({ url: url, posts: result.rss.channel[0].item })
+				})
+			})
+			.catch((error) => {
+				reject(error)
+			})
+	})
+}
